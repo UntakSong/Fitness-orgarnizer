0dc03a1a99fcd2deb8e7c8dc68c2b6b042c7ff85
UntakSong <75886905+UntakSong@users.noreply.github.com>
2025-08-15 08:34:03 +0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,37 @@
-const CACHE = 'fc-pwa-v5';
-const ASSETS = ['./','./index.html','./manifest.json','./icons/icon-192.png','./icons/icon-512.png'];
-self.addEventListener('install', e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
-self.addEventListener('activate', e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))))});
-self.addEventListener('fetch', e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
+// V5.7 PWA Service Worker (cache + update)
+const VER = 'fc-v5-pwa-2';
+const CORE = ['/', '/index.html', '/manifest.webmanifest', '/icons/icon-192.png', '/icons/icon-512.png'];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(VER).then(c => c.addAll(CORE)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== VER).map(k => caches.delete(k))))
+    .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  const req = e.request;
+  if (req.method !== 'GET') return;
+  const isNav = req.mode === 'navigate';
+  if (isNav) {
+    e.respondWith(
+      fetch(req).then(res => {
+        const copy = res.clone();
+        caches.open(VER).then(c => c.put('/', copy));
+        return res;
+      }).catch(() => caches.match('/') || caches.match('/index.html'))
+    );
+    return;
+  }
+  e.respondWith(
+    caches.match(req).then(hit => hit || fetch(req).then(res => {
+      const copy = res.clone();
+      caches.open(VER).then(c => c.put(req, copy));
+      return res;
+    }))
+  );
+});
